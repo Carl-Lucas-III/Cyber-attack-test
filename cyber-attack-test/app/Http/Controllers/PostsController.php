859d081c8ada57c11d68f01ac04440b719ac87fd
Linus-web <sunil.andersson@gmail.com>
2023-03-24 13:55:06 +0100

    even more security

--- cyber-attack-test/app/Http/Controllers/PostsController.php
+++ cyber-attack-test/app/Http/Controllers/PostsController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rules;
+use Illuminate\Validation\Rules\Unique;
+use Illuminate\Validation\ValidationException as ValidationException;
 
 class PostsController extends Controller
 {
@@ -14,8 +20,15 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return View('Posts', ['posts' =>Post::select()->orderBy('created_at','DESC')->paginate(10)]);
-        
+        try {
+
+            $posts = Post::select('id','title','description','user_id')->orderBy('created_at', 'DESC')->paginate(10);
+            return view('Posts', compact('posts'));
+        } catch (\Exception $e) {
+            // Log the error or display a custom error page
+            Log::error($e->getMessage());
+            abort(500, 'An error occurred while retrieving posts.');
+        }
     }
 
     /**
@@ -23,7 +36,13 @@ class PostsController extends Controller
      */
     public function create()
     {
-        return view('CreatePost');
+        try {
+            return view('CreatePost');
+        } catch (\Exception $e) {
+            // Log the error or display a custom error page
+            \Illuminate\Support\Facades\Log::error($e->getMessage());
+            abort(500, 'An error occurred while loading the create post page.');
+        }
     }
 
     /**
@@ -31,90 +50,146 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        $validated = $request->validate([
-            'title' => 'required|string|unique:posts',
-            'description' => 'required|string'
-        ]);
-        
-        $post = new Post();
-        $post->title = htmlentities(trim(strip_tags($validated['title'])));
-        $post->description = htmlentities(trim(strip_tags($validated['description'])));
-        $post->user_id = Auth::user()->id;
-        
-        $post->save();
-        
-        return redirect()->route('posts.index');
+        try {
+            $validated = $request->validate([
+                'title' => [
+                    'required',
+                    'string',
+                    'max:255',
+                    (new Unique('posts'))->where(function ($query) {
+                        return $query->where('user_id', Auth::id());
+                    })->ignore($request->post)
+                ],
+                'description' => 'required|string|min:10'
+            ], [
+                'title.required' => 'Please enter a title for your post.',
+                'title.max' => 'The title may not be longer than :max characters.',
+                'title.unique' => 'You have already created a post with that title.',
+                'description.required' => 'Please enter a description for your post.',
+                'description.min' => 'The description must be at least :min characters long.'
+            ]);
+            
+            $post = new Post();
+            $post->title = htmlentities(trim(strip_tags($validated['title'])));
+            $post->description = htmlentities(trim(strip_tags($validated['description'])));
+            $post->user_id = Auth::user()->id;
+            
+            $post->save();
+            
+            return redirect()->route('posts.index');
+        } catch (ValidationException $e) {
+            return redirect()->back()->withErrors($e->errors())->withInput();
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            return redirect()->back()->with('error', 'There was an error creating your post. Please try again later.');
+        }
     }
+    
 
     /**
      * Display the specified resource.
      */
     public function show(Post $Post)
-    {
-        return $Post;
+    {   
+        return ;
     }
 
     /**
      * Show the form for editing the specified resource.
      */
     public function edit(Post $Post)
-    {
+{
+    try {
         if (!$Post) {
-            return response()->json([
-                'message' => 'Post not found'
-            ], 404);
+            throw new Exception('Post not found');
         }
     
-        if ($Post->user_id == Auth::user()->id) {
-            return view('UpdatePost', ['post' => $Post]);
-        } else {
-            return response()->json([
-                'message' => 'You do not own this post'
-            ], 403);
+        if ($Post->user_id != Auth::user()->id) {
+            throw new Exception('You do not own this post');
         }
+    
+        return view('UpdatePost', ['post' => $Post]);
+    } catch (Exception $e) {
+        Log::error($e->getMessage());
+        return redirect()->back()->with('error', $e->getMessage());
     }
+}
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Post $Post)
     {
-        // validate the request inputs
-    $validatedData = $request->validate([
-        'title' => 'sometimes|max:255',
-        'description' => 'sometimes',
-    ]);
+        try {
+            // validate the request inputs
+            $validatedData = $request->validate([
+                'title' => [
+                    'required',
+                    'string',
+                    'max:255',
+                    (new Unique('posts'))->where(function ($query) {
+                        return $query->where('user_id', Auth::id());
+                    })->ignore($request->post)
+                ],
+                'description' => 'required|string|min:10'
+            ], [
+                'title.required' => 'Please enter a title for your post.',
+                'title.max' => 'The title may not be longer than :max characters.',
+                'title.unique' => 'You have already created a post with that title.',
+                'description.required' => 'Please enter a description for your post.',
+                'description.min' => 'The description must be at least :min characters long.'
+            ]);
     
-    // sanitize the data
-    if($validatedData['title'])
-    $title = htmlentities(strip_tags(trim($validatedData['title'])));
-    if($validatedData['description'])
-    $description = htmlentities(strip_tags(trim($validatedData['description'])), ENT_QUOTES);
-
-    // update the post with sanitized data
-
-    $Post->update([
-        'title' => $validatedData['title']?$title:$Post->title,
-        'description' => $validatedData['description']?$description:$Post->description,
-    ]);
-
-    return redirect()->route('posts.index');
+            // sanitize the data
+            if($validatedData['title'])
+                $title = htmlentities(strip_tags(trim($validatedData['title'])));
+            if($validatedData['description'])
+                $description = htmlentities(strip_tags(trim($validatedData['description'])), ENT_QUOTES);
+    
+            // update the post with sanitized data
+            $Post->update([
+                'title' => $validatedData['title'] ? $title : $Post->title,
+                'description' => $validatedData['description'] ? $description : $Post->description,
+            ]);
+    
+            return redirect()->route('posts.index');
+    
+        } catch (ValidationException $e) {
+            return redirect()->back()->withErrors($e->errors())->withInput();
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            return redirect()->back()->with('error', 'There was an error updating your post. Please try again later.');
+        }
     }
-
+    
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Post $Post)
-    {
-
-        
-        if($Post->user_id == Auth::user()->id){
-   
-            $Post->delete();
-            
-            return redirect()->back();
+{
+    try {
+        if (!$Post) {
+            throw new ModelNotFoundException('Post not found');
         }
-        else
-        return redirect()->back()->withErrors('this post is not yours')->withInput();
+        
+        if ($Post->user_id != Auth::user()->id) {
+            throw new AuthorizationException('You do not own this post');
+        }
+        
+        $Post->delete();
+        
+        return redirect()->back();
+    } catch (ModelNotFoundException $e) {
+        return response()->json([
+            'message' => $e->getMessage()
+        ], 404);
+    } catch (AuthorizationException $e) {
+        return response()->json([
+            'message' => $e->getMessage()
+        ], 403);
+    } catch (Exception $e) {
+        Log::error($e->getMessage());
+        return redirect()->back()->with('error', 'There was an error deleting your post. Please try again later.');
     }
+}
 }
--- cyber-attack-test/resources/views/CreatePost.blade.php
+++ cyber-attack-test/resources/views/CreatePost.blade.php
@@ -7,35 +7,35 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center align-middle">
+            <form method="POST" action="{{ route('posts.store') }}">
+                @csrf
+                @method('POST')
 
-                <form method="POST" action="{{ route('posts.store') }}">
-                    @csrf
-            
-            
-                    <!-- Email Address -->
-                    <div>
-                        <x-input-label for="email" :value="__('Title')" />
-                        <x-text-input id="email" class="block mt-1 w-full" type="text" name="title" />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                    </div>
-            
-                    <!-- Password -->
-                    <div class="mt-4">
-                        <x-input-label for="password" :value="__('Description')" />
-                        <x-text-input id="password" class="block mt-1 w-full" type="text" name="description"  />
-                        <x-input-error :messages="$errors->get('description')" class="mt-2" />
-                    </div>
-            
-                   
-            
-                    <div class="flex items-center justify-end mt-4">
-                        <x-primary-button>
-                            {{ __('Create Post') }}
-                        </x-primary-button>
-                    </div>
-                </form>
-            
+                <!-- Title -->
+                <div>
+                    <x-input-label for="title" value="{{ __('Title') }}" />
+                    <x-text-input id="title" type="text" name="title" />
+                    @error('title')
+                        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                    @enderror
+                </div>
 
+                <!-- Description -->
+                <div class="mt-4">
+                    <x-input-label for="description" value="{{ __('Description') }}" />
+                    <textarea name="description" id="description" rows="5" class="form-input rounded-md shadow-sm mt-1 block w-full"></textarea>
+                    @error('description')
+                        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <!-- Submit Button -->
+                <div class="flex items-center justify-end mt-4">
+                    <x-primary-button type="submit" class="ml-3">
+                        {{ __('Create Post') }}
+                    </x-primary-button>
+                </div>
+            </form>
         </div>
     </div>
 </x-app-layout>
